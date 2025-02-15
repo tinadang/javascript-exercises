8618b21d35f354e287a6d98e4654c29a8cc99995
Tina Dang <tdtinadang@gmail.com>
2025-02-14 21:45:24 -0800

    Finish repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    let newString = "";
+
+    for (let i = 0; i < times; i++) {
+        newString += string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
