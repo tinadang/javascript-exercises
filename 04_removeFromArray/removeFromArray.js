594b3da28eb24f99ed3d45d603b47f0a3d4a2b21
Tina Dang <tdtinadang@gmail.com>
2025-02-17 20:29:19 -0800

    Finish removeFromArray exercise with method 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+
+    //Method 1: Create and return a new array with targets removed
+    let newArray = [];
+
+    checkArray:
+    for (let i = 0; i < array.length; i++) {
+        for (const target of targets) {
+            if (array[i] === target) {
+                continue checkArray;
+            }
+        }
+        newArray.push(array[i]);
+    }
+
+    return newArray;
+
 };
 
 // Do not edit below this line
