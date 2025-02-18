1418f403cb547f07382db2d785acfb6b3c1e65ff
Tina Dang <tdtinadang@gmail.com>
2025-02-18 00:41:15 -0800

    Finish tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+  const roundedCelsius = Math.round(celsiusTemp * 10) / 10;
+
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrenheitTemp = (celsiusTemp * (9 / 5)) + 32;
+  const roundedFahrenheit = Math.round(fahrenheitTemp * 10) / 10;
+
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
