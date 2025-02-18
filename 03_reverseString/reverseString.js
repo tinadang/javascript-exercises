abe01494faf4fc3e491fb9cbb3c9adb0ce74b355
Tina Dang <tdtinadang@gmail.com>
2025-02-17 20:02:25 -0800

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
 
+    let reversedString = "";
+    for (let i = string.length-1; i >= 0; i--) {
+        reversedString += stringArray[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
