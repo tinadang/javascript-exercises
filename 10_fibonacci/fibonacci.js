d61527ea0c266feb87152fe2ab3f53d6d9feeef7
Tina Dang <tdtinadang@gmail.com>
2025-02-18 19:57:19 -0800

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(numberInSeries) {
+    if (numberInSeries < 0) {
+        return "OOPS";
+    }
 
+    let sequence = [0, 1];
+
+    for (let i = 1; i <= +numberInSeries; i++) {
+        sequence.push(sequence[i-1] + sequence[i]);
+    }
+
+    return sequence[numberInSeries];
 };
 
 // Do not edit below this line
