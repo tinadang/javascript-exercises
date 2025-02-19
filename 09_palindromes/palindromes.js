98dddb18b1feaf731c851427cd4fdee9638d6490
Tina Dang <tdtinadang@gmail.com>
2025-02-18 19:40:43 -0800

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //Change word to lowercase, split into array of characters, filter valid characters
+    let wordArray = word
+    .toLowerCase()
+    .split("")
+    .filter((letter) => /[a-z0-9]/.test(letter));
+
+    //Create a variable to hold a reversed word array
+    let reversedWordArray = wordArray.toReversed();
+
+    //Test if each character in each array match with each other
+    return wordArray.every((letter, index) => letter === reversedWordArray[index]);
 
 };
 
